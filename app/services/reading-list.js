ff8ae354e29235e0f5cac53638bd95027649daf0
fedekau <fede.kau@gmail.com>
2018-02-03 16:42:18 -0300

    Show basic tweets list

--- app/services/reading-list.js
+++ app/services/reading-list.js
@@ -1,4 +1,27 @@
 import Service from '@ember/service';
 
 export default Service.extend({
+  list() {
+    return new Promise((resolve, reject) => {
+      chrome.storage.sync.get({ readingList: [] }, function (result) {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve(result.readingList);
+        }
+      })
+    });
+  },
+
+  clear() {
+    return new Promise((resolve, reject) => {
+      chrome.storage.sync.clear(() => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
 });
